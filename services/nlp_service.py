ae72a7c125dbdf928d0ccb2233b1b1401f1540df
aarushi <aaarushi817@gmail.com>
2026-04-14 21:00:47 +0530

    services lazy load

--- services/nlp_service.py
+++ services/nlp_service.py
@@ -1,6 +1,12 @@
 from transformers import pipeline
 
-sentiment_model = pipeline("sentiment-analysis")
+sentiment_model = None
+
+def get_sentiment_model():
+    global sentiment_model
+    if sentiment_model is None:
+        sentiment_model = pipeline("sentiment-analysis")
+    return sentiment_model
 
 def analyze_sentiment(text):
-    return sentiment_model(text)
+    return get_sentiment_model()(text)
--- services/rag_service.py
+++ services/rag_service.py
@@ -2,7 +2,13 @@ from sentence_transformers import SentenceTransformer
 import faiss
 import numpy as np
 
-model = SentenceTransformer('all-MiniLM-L6-v2')
+model = None
+
+def get_model():
+    global model
+    if model is None:
+        model = SentenceTransformer('all-MiniLM-L6-v2')
+    return model
 
 class RAGSystem:
     def __init__(self):
@@ -11,11 +17,11 @@ class RAGSystem:
 
     def add_documents(self, docs):
         self.texts.extend(docs)
-        embeddings = model.encode(docs)
+        embeddings = get_model().encode(docs)
         self.index.add(np.array(embeddings))
 
     def search(self, query, k=3):
-        q_emb = model.encode([query])
+        q_emb = get_model().encode([query])
         D, I = self.index.search(np.array(q_emb), k)
         return [self.texts[i] for i in I[0]]
 
