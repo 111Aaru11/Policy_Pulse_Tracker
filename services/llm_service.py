be9ba6d9ab9b6df4b73ae3e7d52fe4f2b14808ab
Aarushi Thakur <148994385+111Aaru11@users.noreply.github.com>
2026-05-01 15:00:14 +0530

    Update llm_service.py

--- services/llm_service.py
+++ services/llm_service.py
@@ -1,20 +1,4 @@
-# import requests
 
-# def ask_gemini(prompt):   
-#     url = "http://localhost:11434/api/generate"
-
-#     data = {
-#         "model": "llama3",  
-#         "prompt": prompt,
-#         "stream": False
-#     }
-
-#     try:
-#         response = requests.post(url, json=data)
-#         result = response.json()
-#         return result.get("response", "No response from Ollama")
-#     except:
-#         return "⚠️ Ollama not running. Start it using 'ollama run llama3'"
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
@@ -23,10 +7,10 @@ load_dotenv()
 
 client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
-    api_key=os.getenv("OPENROUTER_API_KEY")   # ✅ FIXED
+    api_key=os.getenv("OPENROUTER_API_KEY") 
 )
 
-def ask_gemini(prompt):   # keep same name (no change in app.py)
+def ask_gemini(prompt):   
     response = client.chat.completions.create(
         model="openai/gpt-3.5-turbo",
         messages=[
